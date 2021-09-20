6968e09ef068f2064bf34b481e0d1fb4a5a30278
LinaMejia <linalmejia@gmail.com>
2021-09-19 23:21:39 -0500

    diseño y funcionalidades

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 import 'package:semanda2flutter1/widgets/botones.widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,97 +10,262 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> operation = [];
+  // String operation = "";
+  String result = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
+      appBar: AppBar(
+        title: Text("Calculadora"),
+      ),
       body: Column(
         children: [
           Expanded(
-            flex: 5,
-            child: Container(
-              child: Row(
-                children: [
-                  Text("zona resultados"),
-                ],
-              ),
-              color: Colors.blue,
-            ))
-          ,
+              flex: 3,
+              child: Container(
+                child: Row(
+                  children: [
+                    Text(result),
+                  ],
+                ),
+                color: Colors.grey.shade900,
+              )),
           Expanded(
-            flex: 1,
-            child: Container(
-              child: Row(
-                children: [
-                  Text("zona ingreso"),
-                ],
-              ),
-              color: Colors.red,
-            ))
-            ,
-          Expanded( 
-            flex: 2,
-            child: Container(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: _listaBotonesFila1Prueba()),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: _listaBotonesFila1Prueba()),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: _listaBotonesFila1Prueba()),
-                 Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: _listaBotonesFila1Prueba()),
-                ],
-              ),
-              color: Colors.green,
-            ))
-          ,
+              flex: 1,
+              child: Container(
+                child: Row(
+                  children: [
+                    Text(operation.join("")),
+                  ],
+                ),
+                // color: Colors.red,
+              )),
+          Expanded(
+              flex: 2,
+              child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotonesFila1Prueba()),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotonesFila2()),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotonesFila3()),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotonesFila4()),
+                  ],
+                ),
+                // color: Colors.green,
+              )),
         ],
       ),
     );
   }
 
-
   _listaBotonesFila1Prueba() {
     return [
-      pintarBoton(metodo: (){}, compoente:  Text("7"), colorBoton: Colors.yellow),
-      pintarBoton(metodo: (){}, compoente:  Icon(Icons.arrow_back_ios)),
-      pintarBoton(metodo: (){}, compoente:  Text("8")),
-      pintarBoton(metodo: (){}, compoente:  Text("9")),
-      pintarBoton(metodo: (){print("object");}, valorFlex: 4, compoente:  Text("=")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("7");
+            });
+          },
+          compoente: Text("7")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("8");
+            });
+          },
+          compoente: Text("8")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("9");
+            });
+          },
+          compoente: Text("9")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("÷");
+            });
+          },
+          compoente: Text("÷")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              if (operation.isNotEmpty) {
+                operation.removeLast();
+              }
+            });
+          },
+          compoente: Icon(Icons.backspace)),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation = [];
+            });
+          },
+          compoente: Text("C")),
     ];
   }
 
-  _listaBotonesFila1() {
+  _listaBotonesFila2() {
     return [
-      ElevatedButton(
-        onPressed: (){}, 
-        child: Text("7")
-      )
-      ,ElevatedButton(
-        onPressed: (){}, 
-        child: Text("8")
-      ),
-      ElevatedButton(
-        onPressed: (){}, 
-        child: Text("9")
-      ),
-      ElevatedButton(
-        onPressed: (){}, 
-        child: Text("÷")
-      ),
-      ElevatedButton(
-        onPressed: (){}, 
-        child: Icon(Icons.arrow_back)
-      ),
-      ElevatedButton(
-        onPressed: (){}, 
-        child: Text("C")
-      )];
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("4");
+            });
+          },
+          compoente: Text("4")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("5");
+            });
+          },
+          compoente: Text("5")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("6");
+            });
+          },
+          compoente: Text("6")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("*");
+            });
+          },
+          compoente: Text("x")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("(");
+            });
+          },
+          compoente: Text("(")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add(")");
+            });
+          },
+          compoente: Text(")")),
+    ];
+  }
+
+  _listaBotonesFila3() {
+    return [
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("1");
+            });
+          },
+          compoente: Text("1")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("2");
+            });
+          },
+          compoente: Text("2")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("3");
+            });
+          },
+          compoente: Text("3")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("-");
+            });
+          },
+          compoente: Text("-")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("²");
+            });
+          },
+          compoente: Text("x²")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("√(");
+            });
+          },
+          compoente: Text("√")),
+    ];
+  }
+
+  _listaBotonesFila4() {
+    return [
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("0");
+            });
+          },
+          compoente: Text("0")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add(",");
+            });
+          },
+          compoente: Text(",")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("%");
+            });
+          },
+          compoente: Text("%")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              operation.add("+");
+            });
+          },
+          compoente: Text("+")),
+      pintarBoton(
+          metodo: () {
+            setState(() {
+              String oper = operation.join("");
+              try {
+                String clean_oper = oper
+                    .replaceAll("√", "sqrt")
+                    .replaceAll("²", "^2")
+                    .replaceAll("÷", "/")
+                    .replaceAll(",", ".");
+                Parser p = Parser();
+                Expression exp = p.parse(clean_oper);
+                ContextModel cm = ContextModel();
+                final resultStr =
+                    exp.evaluate(EvaluationType.REAL, cm).toString();
+                result = "${result} \n${oper}=${resultStr}  ";
+                operation = [];
+              } catch (e) {
+                result = "${result} \n${oper} :${e.toString().split(":")[1]}";
+              }
+            });
+          },
+          valorFlex: 2,
+          compoente: Text("="),
+          colorBoton: Colors.green),
+    ];
   }
 }
--- lib/widgets/botones.widget.dart
+++ lib/widgets/botones.widget.dart
@@ -1,19 +1,22 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-Widget pintarBoton({required dynamic metodo, int valorFlex = 1 , required Widget compoente, dynamic colorBoton = Colors.red }){
-    return Expanded(
-        flex: valorFlex,
-        child: Container(
-          child: 
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: ElevatedButton(
+Widget pintarBoton(
+    {required dynamic metodo,
+    int valorFlex = 1,
+    required Widget compoente,
+    dynamic colorBoton}) {
+  return Expanded(
+      flex: valorFlex,
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                primary: colorBoton,
-              ),
-              onPressed: metodo, 
-              child: compoente
-            ),
-          ),
+                  primary: colorBoton != null
+                      ? colorBoton
+                      : ThemeData.dark().backgroundColor),
+              onPressed: metodo,
+              child: compoente),
+        ),
       ));
-  }
+}
